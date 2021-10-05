7eaeccf77d72607f3d6ef90ecbf6fcb843579ce2
John Pitchko <john.pitchko@icloud.com>
2021-10-05 09:26:49 -0600

    Enable NetlifyCMS

---
+++ assets/js/netlifycms_redirect.js
@@ -0,0 +1,11 @@
+// https://www.netlifycms.org/docs/add-to-your-site/
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
